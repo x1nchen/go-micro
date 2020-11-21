6a23bd5c54642055757054369022bec7da723c8d
Asim Aslam <asim@aslam.me>
2020-11-21 06:57:27 +0000

    Rename

--- app/program.go
+++ app/program.go
@@ -7,12 +7,12 @@ type Program interface {
 	Name(string)
 	// Request an application by name and endpoint
 	Request(name, ep string, req, rsp interface{}) error
-	// Broadcast a message to all subscribers
-	Broadcast(topic string, msg interface{}) error
+	// Broadcast a message to a group
+	Broadcast(group string, msg interface{}) error
 	// Register a handler e.g a public Go struct/method with signature func(context.Context, *Request, *Response) error
 	Handle(v interface{}) error
 	// Subscribe to broadcast messages. Signature is public Go func or struct with signature func(context.Context, *Message) error
-	Subscribe(topic string, v interface{}) error
+	Subscribe(group string, v interface{}) error
 	// Run the application
 	Run() error
 }
